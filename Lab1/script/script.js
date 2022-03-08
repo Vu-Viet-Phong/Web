6f524d2f5c3144fdc78389f3849c72b5b66769c2
vtenpo <phongzed2001@gmail.com>
2022-03-08 19:58:57 +0700

    commit

--- Lab1/script/script.js
+++ Lab1/script/script.js
@@ -11,11 +11,11 @@ function addEltToSVG(svg, name, attrs)	{
 }
     
 function createHistogram(svg, name) {
-    var arr = [0,0,0,0,0,0]; 
+    var arr = [0, 0, 0, 0, 0, 0]; 
 
     name = name.toLowerCase();
                     
-    for (var i = 0; i < name.length; i++) {
+    for (var i in arr) {
         var code = name.charCodeAt(i);
         if (code >= 97 && code < 101) arr[0]++;
         if (code >= 101 && code < 105) arr[1]++;
@@ -25,8 +25,8 @@ function createHistogram(svg, name) {
         if (code >= 118 && code < 123) arr[5]++;
     }
         
-    for (var i in arr) {
-        var attrs = {"x": 50 * i, "y": 165, "height": arr[i] * 50, "width": 50, "fill": "blue", "stroke": "black"};
+    for (var value in arr) {
+        var attrs = {"x": 50 * value, "y": 165, "height": arr[value] * 50, "width": 50, "fill": "blue", "stroke": "black"};
         if (attrs.height === 0) attrs.height = 1;
         addEltToSVG(svg, "rect", attrs);
     }                
