3fa4b962eed32ab91d5633b7ad85832ae63d0486
vtenpo <phongzed2001@gmail.com>
2022-03-22 13:57:51 +0700

    commit

--- d3-book/script/barchart-sort.js
+++ d3-book/script/barchart-sort.js
@@ -1,9 +1,20 @@
-// Width and height
-var w = 600;
-var h = 250;
+function generate(numValues, max_value) {
+    var data = []; // Initialize empty array
+    
+    for (var i = 0; i < numValues; i++) {                       // Loop numValues times
+        var newNumber = Math.floor(Math.random() * max_value);  // New random integer (0-max_value)
+        data.push(newNumber);                                   // Add new number to array
+    }
 
-var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
-                11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+    return data;
+}
+
+// Width and height
+var w = 800;
+var h = 300;
+
+var maxValue = 30; // Highest possible new value
+var dataset = generate(30, maxValue);
 
 var xScale = d3.scaleBand()
                .domain(d3.range(dataset.length))
@@ -47,15 +58,22 @@ svg.selectAll("text")
 // On click, update with new data			
 d3.select("p").on("click", function() {
     // New values for dataset
-    dataset = [ 11, 12, 15, 20, 18, 17, 16, 18, 23, 25,
-                5, 10, 13, 19, 21, 25, 22, 18, 15, 13 ];
+    var numValues = dataset.length; // Count original length of dataset
+    var newMaxValue = 100;	            // Highest possible new value
+    dataset = generate(numValues, newMaxValue);
+
+    // Update scale domain
+	// Recalibrate the scale domain, given the new max value in dataset
+    yScale.domain([0, d3.max(dataset)]);
 
     // Update all rects
     svg.selectAll("rect")
        .data(dataset)
-       .transition()    // <-- This makes it a smooth transition!
-       .duration(2000)  // <-- Controlled duration, 2000 = 2s
-       .ease(d3.easeCircleIn) // easeLinear/easeCircleIn/easeElasticOut/easeBounceOut
+       .transition() // <-- This makes it a smooth transition!
+    // .delay(500)   // <-- A static, 1s delay before transition begins
+       .delay(function(d, i) { return i * 200; }) // One-tenth of an additional second delay for each subsequent element 
+       .duration(500)          // <-- Controlled duration, 2000 = 2s
+    // .ease(d3.easeCircleIn)   // easeLinear/easeCircleIn/easeElasticOut/easeBounceOut
        .attr("y", function(d) { return h - yScale(d); })
        .attr("height", function(d) { return yScale(d); })
        .attr("fill", function(d) { return "rgb(0, 0, " + Math.round(d * 10) + ")"; });
@@ -64,8 +82,10 @@ d3.select("p").on("click", function() {
     svg.selectAll("text")
        .data(dataset)
        .transition()
-       .duration(2000)
-       .ease(d3.easeCircleIn)
+       .duration(500)
+    // .delay(500)
+       .delay(function(d, i) { return i * 200; }) // One-tenth of an additional second delay for each subsequent element 	
+    // .ease(d3.easeCircleIn)
        .text(function(d) { return d; })
        .attr("x", function(d, i) { return xScale(i) + xScale.bandwidth() / 2; })
        .attr("y", function(d) { return h - yScale(d) + 14; });
