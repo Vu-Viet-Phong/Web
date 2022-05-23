28bd65c621769680ddfcb197bbc62b7c7e66d6dc
vtenpo <phongzed2001@gmail.com>
2022-05-23 12:16:04 +0700

    commit

--- Data-Science-and-Data-Visualization/labs/Lab7/script/script.js
+++ Data-Science-and-Data-Visualization/labs/Lab7/script/script.js
@@ -5,17 +5,20 @@ var rowConverter = function(d) {
   };
 }
 
-d3.csv("https://raw.githubusercontent.com/vtenpo/Web/main/Data-Science-and-Data-Visualization/labs/Lab7/data/vn-provinces-data.csv", 
-  rowConverter, function(data, error) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(data);
-      geomapping(data);
-    }
+d3.csv("https://raw.githubusercontent.com/vtenpo/Web/main/Data-Science-and-Data-Visualization/labs/Lab7/data/vn-provinces-data.csv", rowConverter, function(data, error) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+    geomapping(data);
+  }
 });
 
 function geomapping(data) {
   var dataset = data;
-  
+
+  d3.json("https://raw.githubusercontent.com/vtenpo/Web/main/Data-Science-and-Data-Visualization/labs/Lab7/json/vn-provinces.json", function(json) {
+
+  });
+
 }
