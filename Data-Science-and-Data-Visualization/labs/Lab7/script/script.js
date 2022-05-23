b97e554bcf7a179b1be7bd36729b6a9a64cef7ea
vtenpo <phongzed2001@gmail.com>
2022-05-23 11:47:06 +0700

    commit

--- Data-Science-and-Data-Visualization/labs/Lab7/script/script.js
+++ Data-Science-and-Data-Visualization/labs/Lab7/script/script.js
@@ -1,5 +1,21 @@
 var rowConverter = function(d) {
-    returns {
-        
+  return {
+    ma: parseFloat(d.ma),
+    area: parseFloat(d.area / 1000),
+  };
+}
+
+d3.csv("https://raw.githubusercontent.com/vtenpo/Web/main/Data-Science-and-Data-Visualization/labs/Lab7/data/vn-provinces-data.csv", 
+  rowConverter, function(data, error) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+      geomapping(data);
     }
+});
+
+function geomapping(data) {
+  var dataset = data;
+  
 }
