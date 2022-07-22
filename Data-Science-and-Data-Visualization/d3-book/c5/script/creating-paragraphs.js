7e12bf89898209db27b5c4b8d6281189dc626a17
vtenpo <phongzed2001@gmail.com>
2022-07-22 14:51:59 +0700

    creating paragraph

--- Data-Science-and-Data-Visualization/d3-book/c5/script/creating-paragraphs.js
+++ Data-Science-and-Data-Visualization/d3-book/c5/script/creating-paragraphs.js
@@ -4,5 +4,14 @@ d3.select("body").selectAll("p")
   .data(dataset)
   .enter()
   .append("p")
-  .text("New Paragraph!");
+  .text(function(d) {
+    return "I can count up to " + d;
+  })
+  .style("color", function(d) {
+    if (d > 15) { // Threshold of 15
+      return "red";
+    } else {
+      return "black";
+    }
+  });
     
